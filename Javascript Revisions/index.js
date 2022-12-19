4194538215d85e56a4b58d43816c89be917af971
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-19 21:38:52 +0530

    class and inheritance

--- Javascript Revisions/index.js
+++ Javascript Revisions/index.js
@@ -21,3 +21,29 @@ const myLastName=(name,age)=>{
 console.log(myLastName("Hamzah",26));
 const square=(num)=>num*num;
 console.log(square(10));
+
+//class and inheritance
+
+class Human {
+  constructor() {
+    this.species = "human beings";
+  }
+  category() {
+    console.log("we are all ", this.species);
+  }
+}
+class Person extends Human {
+    constructor() {
+        super();
+        this.name="Hamzah Aslam Ansari";
+        this.species="Aadam Zaat"//we can now access object and properties now from parent class
+    }
+    details(){
+        console.log("My name is ",this.name);
+    }
+}
+// const breed=new Human();
+// breed.category()
+const person=new Person();
+person.category();//we can access methods of parent class now
+person.details();
