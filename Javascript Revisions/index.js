4ee14330d1a1f549efa1fca10e99ff07304aecf8
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-19 20:01:58 +0530

    diff bw array function and normal js function

--- Javascript Revisions/index.js
+++ Javascript Revisions/index.js
@@ -6,6 +6,18 @@ let goat="Ronaldo";
 goat="Messi"
 // console.log(`Goat is ${goat}`);//accepted
 const bestEuropenPlayer="Ronaldo";
-bestEuropenPlayer="Messi"
+// bestEuropenPlayer="Messi"
 // console.log(`Goat Eueoopean  is ${goat}`);//thorws erro
 
+//diff between arrow function and norml function
+function myName(name){
+    return `my  initial name is ${name}`
+}
+console.log(myName("Hamzah"));
+
+const myLastName=(name,age)=>{
+ return  `my  last name is ${name} and age is ${age}`
+}
+console.log(myLastName("Hamzah",26));
+const square=(num)=>num*num;
+console.log(square(10));
