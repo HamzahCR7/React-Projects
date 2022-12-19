d7e3cf14124728549c888ffb7b07980d4cb2db60
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-19 19:45:29 +0530

    diff between let,var and const

--- Javascript Revisions/index.js
+++ Javascript Revisions/index.js
@@ -1,1 +1,11 @@
-console.log("Initial commit");
+//diff between let and const
+var name="Hamzah";
+name="Messi"
+// console.log(name)//accepted
+let goat="Ronaldo";
+goat="Messi"
+// console.log(`Goat is ${goat}`);//accepted
+const bestEuropenPlayer="Ronaldo";
+bestEuropenPlayer="Messi"
+// console.log(`Goat Eueoopean  is ${goat}`);//thorws erro
+
