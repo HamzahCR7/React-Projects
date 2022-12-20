2660f8ce75e2b27fff662d65a3e1915680d81603
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-20 23:32:05 +0530

    initial files setup

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">       
+        <a >
+          Expense tracker
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
