9e4e840ab9a832301e7d95260d855f8991e5e317
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-26 22:51:43 +0530

    date logic put in separate component and bifurcating app into components structure as well as addes css for date compoenent

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,15 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props){
-    const month=props.currentDate.toLocaleString('en-IN',{month:'long'});
-    const day=props.currentDate.toLocaleString('en-IN',{day:'2-digit'});
-    const year=props.currentDate.getFullYear();
+    
     return (
         <div className='expense-item' key={props.id}>
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate currentDate={props.currentDate}/>
                 <div className='expense-item__description'>
                     <h2>{props.title}</h2>
                     <div className='expense-item__price'>$ {props.price}</div>
