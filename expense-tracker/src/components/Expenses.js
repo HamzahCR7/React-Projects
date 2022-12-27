d7bce9e3ade957875a91459c35b5541022be6de6
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-27 22:49:43 +0530

    Removing expenseItem from app and calling it from new component.Instead of repeating array using map function to render data

---
+++ expense-tracker/src/components/Expenses.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+function Expenses(props){
+    
+      return (
+        <div className="expenses">
+          {/* <ExpenseItem
+            title={props.items[0].title}
+            price={props.items[0].price}
+            currentDate={props.items[0].currentDate}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={props.items[1].title}
+            price={props.items[1].price}
+            currentDate={props.items[1].currentDate}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={props.items[2].title}
+            price={props.items[2].price}
+            currentDate={props.items[2].currentDate}
+          ></ExpenseItem> */}
+          {
+            props.items.map((data,index)=>(
+                <ExpenseItem key={index} title={data.title} price={data.price} currentDate={data.currentDate}/>
+            ))
+          }
+        </div>
+      );
+
+}
+export default Expenses;
