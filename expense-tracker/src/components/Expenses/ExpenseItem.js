7fc1d7922658eed8a2a9d95093e802ccbf33b7b4
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-28 22:13:09 +0530

    adding event in component

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,9 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 const ExpenseItem = (props)=>{
-    
+    const onCLickHandler=()=>{
+        console.log('clicked')
+    }
     return (
         <Card className='expense-item' key={props.id}>
             <ExpenseDate currentDate={props.currentDate}/>
@@ -10,6 +12,7 @@ const ExpenseItem = (props)=>{
                     <h2>{props.title}</h2>
                     <div className='expense-item__price'>$ {props.price}</div>
                 </div>
+                <button onClick={onCLickHandler}>Change Name</button>
             </Card>
     )
 }
