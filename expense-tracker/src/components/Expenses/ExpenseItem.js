b60b054c2dbd8adf74da18e227364573c1c75bbc
hamzacr7 <hamzahaslam523@gmail.com>
2022-12-28 22:28:43 +0530

    concept of state lifecycle hook,it re evaluates the function for any event changes

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
+import React,{useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 const ExpenseItem = (props)=>{
+    const [title,setTitle]=useState(props.title);
+    const [price,setPrice]=useState(props.price);
+
     const onCLickHandler=()=>{
-        console.log('clicked')
+        setTitle('UpDated!');
+        setPrice('Price updated');
     }
     return (
         <Card className='expense-item' key={props.id}>
             <ExpenseDate currentDate={props.currentDate}/>
                 <div className='expense-item__description'>
-                    <h2>{props.title}</h2>
-                    <div className='expense-item__price'>$ {props.price}</div>
+                    <h2>{title}</h2>
+                    <div className='expense-item__price'>$ {price}</div>
                 </div>
                 <button onClick={onCLickHandler}>Change Name</button>
             </Card>
